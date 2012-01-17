20b9b33f7e53f9af45dc10fe1cc74d9afc04b8da
ArthurLehdermann@gmail.com <ArthurLehdermann@gmail.com@0e39aa65-9f6c-7ed5-d32b-531e9fa8ad37>
2012-01-17 13:25:16 +0000

    Ajuste no php que le as imagens

--- exibe_imagem.php
+++ exibe_imagem.php
@@ -6,5 +6,9 @@ $mimeType = image_type_to_mime_type(exif_imagetype($image_path));
 
 // Exibe a imagem
 header('Content-Type: '.$mimeType);
+$array = explode('/', $image_path);
+$file_name = array_pop($array);
+header('Content-Disposition: attachment; filename="'.$file_name.'";'); 
+header('Content-Transfer-Encoding: binary');
 readfile($image_path);
 ?>
